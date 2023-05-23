687f156fda2c7f34f691649ed1af4639c349a6f6
luatnc <luatncth2007024@fpt.edu.vn>
2023-05-23 10:03:35 -0700

    luatnc-khuyen-nghi-vip

--- resources/views/client/dataFinancial/recommendations/loadlist.blade.php
+++ resources/views/client/dataFinancial/recommendations/loadlist.blade.php
@@ -10,132 +10,30 @@
         <input type="hidden" name="_token" id="_token" value="{{ csrf_token() }}">
         <div class="chat">
             <div  id="messages">
+                @foreach ($datas as $key => $data)
                 <div class="d-flex flex-row justify-content-start mb-4 avatarMessage">
                     <img src="../clients/img/LogoFinTop_red.png"
                         alt="avatar 1" style="width: 30px; height: 100%;">
-                    <div class=" d-flex p-3 ms-3" style="border-radius: 15px; background-color: #e1ebe6;width:80%;font-family:auto">
+                    @if($data->type == 'MUA')
+                    <div class=" d-flex p-3 ms-3" style="border-radius: 15px; background-color: #2a3546;color:#ffd743;width:80%;font-family:auto">
+                    @else
+                    <div class=" d-flex p-3 ms-3" style="border-radius: 15px; background-color: #861515;color:white;width:80%;font-family:auto">
+                    @endif
                        <div style="width:30%">
                         <img src="{{url('/file-image-client/blogs/2023_05_10_1353000000541828!~!image_background.png')}}"
                             alt="avatar 1" style="height: 150px;width: 250px;object-fit: cover;">
                        </div>
                        <div style="padding-left:30px">
-                        <h4>MUA XXX 20% NAV (tài sản)</h4>
+                        <h4>{{ $data->title }}</h4>
                         <p>Giá mua: 56.2 - 57.</p>
-                        <p>Mục tiêu: 60 - 62 - 64</p>
-                        <p>Dừng lỗ:  54</p>
+                        <p>Mục tiêu: {{ $data->target }}</p>
+                        <p>Dừng lỗ:  {{ $data->stop_loss }}</p>
                        </div>
                         
                         <!-- <p style="font-size:14px" class="small mb-0">Chào Anh/Chị, chúng tôi có thể hỗ trợ gì cho Anh/chị ạ?</p> -->
                     </div>
                 </div>
-                <div class="d-flex flex-row justify-content-start mb-4 avatarMessage">
-                    <img src="../clients/img/LogoFinTop_red.png"
-                        alt="avatar 1" style="width: 30px; height: 100%;">
-                    <div class=" d-flex p-3 ms-3" style="border-radius: 15px; background-color: #e1ebe6;width:80%;font-family:auto">
-                       <div style="width:30%">
-                        <img src="{{url('/file-image-client/blogs/2023_05_10_1353000000541828!~!image_background.png')}}"
-                            alt="avatar 1" style="height: 150px;width: 250px;object-fit: cover;">
-                       </div>
-                       <div style="padding-left:30px">
-                        <h4>MUA XXX 20% NAV (tài sản)</h4>
-                        <p>Giá mua: 56.2 - 57.</p>
-                        <p>Mục tiêu: 60 - 62 - 64</p>
-                        <p>Dừng lỗ:  54</p>
-                       </div>
-                        
-                        <!-- <p style="font-size:14px" class="small mb-0">Chào Anh/Chị, chúng tôi có thể hỗ trợ gì cho Anh/chị ạ?</p> -->
-                    </div>
-                </div>
-                <div class="d-flex flex-row justify-content-start mb-4 avatarMessage">
-                    <img src="../clients/img/LogoFinTop_red.png"
-                        alt="avatar 1" style="width: 30px; height: 100%;">
-                    <div class=" d-flex p-3 ms-3" style="border-radius: 15px; background-color: #e1ebe6;width:80%;font-family:auto">
-                       <div style="width:30%">
-                        <img src="{{url('/file-image-client/blogs/2023_05_10_1353000000541828!~!image_background.png')}}"
-                            alt="avatar 1" style="height: 150px;width: 250px;object-fit: cover;">
-                       </div>
-                       <div style="padding-left:30px">
-                        <h4>MUA XXX 20% NAV (tài sản)</h4>
-                        <p>Giá mua: 56.2 - 57.</p>
-                        <p>Mục tiêu: 60 - 62 - 64</p>
-                        <p>Dừng lỗ:  54</p>
-                       </div>
-                        
-                        <!-- <p style="font-size:14px" class="small mb-0">Chào Anh/Chị, chúng tôi có thể hỗ trợ gì cho Anh/chị ạ?</p> -->
-                    </div>
-                </div>
-                <div class="d-flex flex-row justify-content-start mb-4 avatarMessage">
-                    <img src="../clients/img/LogoFinTop_red.png"
-                        alt="avatar 1" style="width: 30px; height: 100%;">
-                    <div class=" d-flex p-3 ms-3" style="border-radius: 15px; background-color: #e1ebe6;width:80%;font-family:auto">
-                       <div style="width:30%">
-                        <img src="{{url('/file-image-client/blogs/2023_05_10_1353000000541828!~!image_background.png')}}"
-                            alt="avatar 1" style="height: 150px;width: 250px;object-fit: cover;">
-                       </div>
-                       <div style="padding-left:30px">
-                        <h4>MUA XXX 20% NAV (tài sản)</h4>
-                        <p>Giá mua: 56.2 - 57.</p>
-                        <p>Mục tiêu: 60 - 62 - 64</p>
-                        <p>Dừng lỗ:  54</p>
-                       </div>
-                        
-                        <!-- <p style="font-size:14px" class="small mb-0">Chào Anh/Chị, chúng tôi có thể hỗ trợ gì cho Anh/chị ạ?</p> -->
-                    </div>
-                </div>
-                <div class="d-flex flex-row justify-content-start mb-4 avatarMessage">
-                    <img src="../clients/img/LogoFinTop_red.png"
-                        alt="avatar 1" style="width: 30px; height: 100%;">
-                    <div class=" d-flex p-3 ms-3" style="border-radius: 15px; background-color: #e1ebe6;width:80%;font-family:auto">
-                       <div style="width:30%">
-                        <img src="{{url('/file-image-client/blogs/2023_05_10_1353000000541828!~!image_background.png')}}"
-                            alt="avatar 1" style="height: 150px;width: 250px;object-fit: cover;">
-                       </div>
-                       <div style="padding-left:30px">
-                        <h4>MUA XXX 20% NAV (tài sản)</h4>
-                        <p>Giá mua: 56.2 - 57.</p>
-                        <p>Mục tiêu: 60 - 62 - 64</p>
-                        <p>Dừng lỗ:  54</p>
-                       </div>
-                        
-                        <!-- <p style="font-size:14px" class="small mb-0">Chào Anh/Chị, chúng tôi có thể hỗ trợ gì cho Anh/chị ạ?</p> -->
-                    </div>
-                </div>
-                <div class="d-flex flex-row justify-content-start mb-4 avatarMessage">
-                    <img src="../clients/img/LogoFinTop_red.png"
-                        alt="avatar 1" style="width: 30px; height: 100%;">
-                    <div class=" d-flex p-3 ms-3" style="border-radius: 15px; background-color: #e1ebe6;width:80%;font-family:auto">
-                       <div style="width:30%">
-                        <img src="{{url('/file-image-client/blogs/2023_05_10_1353000000541828!~!image_background.png')}}"
-                            alt="avatar 1" style="height: 150px;width: 250px;object-fit: cover;">
-                       </div>
-                       <div style="padding-left:30px">
-                        <h4>MUA XXX 20% NAV (tài sản)</h4>
-                        <p>Giá mua: 56.2 - 57.</p>
-                        <p>Mục tiêu: 60 - 62 - 64</p>
-                        <p>Dừng lỗ:  54</p>
-                       </div>
-                        
-                        <!-- <p style="font-size:14px" class="small mb-0">Chào Anh/Chị, chúng tôi có thể hỗ trợ gì cho Anh/chị ạ?</p> -->
-                    </div>
-                </div>
-                <div class="d-flex flex-row justify-content-start mb-4 avatarMessage">
-                    <img src="../clients/img/LogoFinTop_red.png"
-                        alt="avatar 1" style="width: 30px; height: 100%;">
-                    <div class=" d-flex p-3 ms-3" style="border-radius: 15px; background-color: #e1ebe6;width:80%;font-family:auto">
-                       <div style="width:30%">
-                        <img src="{{url('/file-image-client/blogs/2023_05_10_1353000000541828!~!image_background.png')}}"
-                            alt="avatar 1" style="height: 150px;width: 250px;object-fit: cover;">
-                       </div>
-                       <div style="padding-left:30px">
-                        <h4>MUA XXX 20% NAV (tài sản)</h4>
-                        <p>Giá mua: 56.2 - 57.</p>
-                        <p>Mục tiêu: 60 - 62 - 64</p>
-                        <p>Dừng lỗ:  54</p>
-                       </div>
-                        
-                        <!-- <p style="font-size:14px" class="small mb-0">Chào Anh/Chị, chúng tôi có thể hỗ trợ gì cho Anh/chị ạ?</p> -->
-                    </div>
-                </div>
+                @endforeach
             </div>
             </div>
         </div>
