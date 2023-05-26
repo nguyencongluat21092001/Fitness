f125703799e022f965100c25f6cd2f55283d02f6
luatnc <luatncth2007024@fpt.edu.vn>
2023-05-26 04:03:50 -0700

    fix

--- modules/System/Dashboard/Handbook/Repositories/HandbookRepository.php
+++ modules/System/Dashboard/Handbook/Repositories/HandbookRepository.php
@@ -5,11 +5,11 @@ namespace Modules\System\Dashboard\Handbook\Repositories;
 use DB;
 use Modules\Base\Repository;
 use Modules\System\Position\Models\PositionModel;
-use Modules\System\Dashboard\Handbook\Models\HandbookModel;
+use Modules\System\Dashboard\Handbook\Models\HandBookModel;
 
 class HandbookRepository extends Repository
 {
     public function model(){
-        return HandbookModel::class;
+        return HandBookModel::class;
     }
 }
