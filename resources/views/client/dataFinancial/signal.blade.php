8810e1d4bcb86519a726bba28b64a87f97e73c81
luatnc <luatncth2007024@fpt.edu.vn>
2023-05-27 02:12:53 -0700

    luatnc-fix

--- resources/views/client/dataFinancial/signal.blade.php
+++ resources/views/client/dataFinancial/signal.blade.php
@@ -8,7 +8,7 @@
             <input type="hidden" name="_token" id="_token" value="{{ csrf_token() }}">
             <div class="home_index_vnindex pt-1 pb-3" style="background:#ffffff91 !important;border-radius:0px !important">
                 <!-- Chú giải xếp hạng TA/FA -->
-                <div class="home_index_child" style="background:#ffffff91 !important;">
+                <div class="home_index_child" style="background:##ffffffe0 !important;">
                     <div class="col-lg-12" style="padding:10px;">
                         <h1 class="h5 "> I. TÍN HIỆU MUA</h1>
                         <div class="table-responsive py-2">
