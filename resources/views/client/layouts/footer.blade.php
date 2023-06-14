3ddd34a5c6fe0c0cf5373e3cf94a30afd2b965b1
khuong1542 <79191404+khuong1542@users.noreply.github.com>
2023-06-15 00:31:56 +0700

    footer

---
+++ resources/views/client/layouts/footer.blade.php
@@ -0,0 +1,162 @@
+<footer class="bgft pt-4" style="background:#121d29bf">
+    <div class="container">
+        <div class="row">
+            <table>
+                <colgroup>
+                    <col width="40%">
+                    <col width="30%">
+                    <col width="30%">
+                </colgroup>
+                <thead>
+                    <th style="vertical-align: middle;">
+                        <h2 class="h4 text-light light-300">
+                            <img src="../clients/img/LogoFinTop_red.png" alt="Card image" width="10%">
+                            <span style="color:#ff0000d1">
+                                <b style="font-family: auto;color:#ff000091;font-weight: 600;animation: lights 10s 750ms linear infinite;">Công ty TNHH Đầu tư & Phát triển FinTop</b>
+                            </span>
+                        </h2>
+                    </th>
+                    <th style="vertical-align: middle;">
+                        <h2 class="h5 text-light light-300"><i class="fas fa-handshake"></i> Đồng hành</h2>
+                    </th>
+                    <th style="vertical-align: middle;">
+                        <h2 class="h5 text-light light-300"><i class="fas fa-handshake"></i> Tải APP</h2>
+                    </th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <!-- Thông tin -->
+                        <td style="vertical-align: top;">
+                            <table class="text-light">
+                                <colgroup>
+                                    <col width="8%">
+                                    <col width="92%">
+                                </colgroup>
+                                <tbody>
+                                    <tr>
+                                        <td align="center" style="vertical-align: middle;"><i class="fas fa-phone-volume"></i></td>
+                                        <td><a class="text-decoration-none pt-2 text-light py-1" href="tel:086.234.8886"> 086.234.8886</a></td>
+                                    </tr>
+                                    <tr>
+                                        <td align="center" style="vertical-align: middle;"><i class="fas fa-envelope"></i></td>
+                                        <td><a class="text-decoration-none pt-2 text-light py-1" href="mailto:FinTop.DVKH@gmail.com"> FinTop.DVKH@gmail.com</a></td>
+                                    </tr>
+                                    <tr>
+                                        <td align="center" style="vertical-align: middle;"><i class="fas fa-map-marker-alt"></i></td>
+                                        <td><span class="text-decoration-none pt-2 text-light py-1"> Trụ sở Hà Nội: 121/26 Thái Hà, P. Trung Liệt, Q. Đống Đa, Hà Nội</span></td>
+                                    </tr>
+                                    <tr>
+                                        <td align="center" style="vertical-align: middle;"><i class="fas fa-house-user"></i></td>
+                                        <td><span class="text-decoration-none pt-2 text-light py-1"> Văn phòng HCM: 383 Võ Văn Tần, P. 5, Q. 3, TP. Hồ Chí Minh</span></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                        <!-- Đồng hành -->
+                        <td style="vertical-align: top;">
+                            <table class="text-light">
+                                <colgroup>
+                                    <col width="8%">
+                                    <col width="92%">
+                                </colgroup>
+                                <tbody>
+                                    <tr>
+                                        <td align="center" style="vertical-align: middle;"><i class="fab fa-facebook"></i></td>
+                                        <td><a class="text-decoration-none pt-2 text-light py-1" href="tel:086.234.8886"> Dữ liệu chứng khoán FinTop.Data</a></td>
+                                    </tr>
+                                    <tr>
+                                        <td align="center" style="vertical-align: middle;"><i class="fab fa-youtube"></i></td>
+                                        <td><a class="text-decoration-none pt-2 text-light py-1" href="https://www.youtube.com/@taichinhdautufintop"> Tài chính & Đầu tư FinTop</a></td>
+                                    </tr>
+                                    <tr>
+                                        <td align="center" style="vertical-align: middle;"><i class="fas fa-hand-point-right"></i></td>
+                                        <td><a class="text-decoration-none pt-2 text-light py-1" href="https://www.youtube.com/@taichinhdautufintop"> Tham gia cộng đồng FinTop </a></td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="2">
+                                            <span>
+                                                <a class="text-light" style="text-decoration: none;" target="_blank" href="http://facebook.com/">
+                                                    <i class='bx bxl-facebook-square bx-md'></i>
+                                                </a>
+                                            </span>
+                                            <span>
+                                                <a class="text-light" style="text-decoration: none;" target="_blank" href="https://www.linkedin.com/">
+                                                    <i class='bx bxl-linkedin-square bx-md'></i>
+                                                </a>
+                                            </span>
+                                            <span>
+                                                <a class="text-light" style="text-decoration: none;" target="_blank" href="https://www.whatsapp.com/">
+                                                    <i class='bx bxl-whatsapp-square bx-md'></i>
+                                                </a>
+                                            </span>
+                                            <span>
+                                                <a class="text-light" style="text-decoration: none;" target="_blank" href="https://www.flickr.com/">
+                                                    <i class='bx bxl-flickr-square bx-md'></i>
+                                                </a>
+                                            </span>
+                                            <span>
+                                                <a class="text-light" style="text-decoration: none;" target="_blank" href="https://www.medium.com/">
+                                                    <i class='bx bxl-medium-square bx-md'></i>
+                                                </a>
+                                            </span>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                        <!-- Tải app -->
+                        <td style="vertical-align: top;">
+                            <table class="text-light">
+                                <colgroup>
+                                    <col width="50%">
+                                    <col width="50%">
+                                </colgroup>
+                                <tbody>
+                                    <tr>
+                                        <td style="vertical-align: top;">
+                                            <table class="text-light">
+                                                <colgroup>
+                                                    <col width="8%">
+                                                    <col width="92%">
+                                                </colgroup>
+                                                <tbody>
+                                                    <tr>
+                                                        <td align="center" style="vertical-align: middle;"><i class="fab fa-app-store-ios"></i></td>
+                                                        <td><a class="text-decoration-none text-light py-1" href="tel:086.234.8886"> App Store </a></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td align="center" style="vertical-align: middle;"><i class="fab fa-google-play"></i></td>
+                                                        <td><a class="text-decoration-none text-light py-1" href="mailto:info@company.com"> Google Play</a></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td align="center" style="vertical-align: middle;"><i class="fas fa-qrcode"></i></td>
+                                                        <td><a class="text-decoration-none text-light py-1" href="mailto:info@company.com"> QR ví momo</a></td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </td>
+                                        <td style="vertical-align: top;">
+                                            <span class="text-decoration-none text-light py-1"> <img class="card-img " src="../clients/img/qrluatnc.jpg" alt="Card image" style="width:40%"> </span>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td></td>
+                                        <td>
+                                            <span class="btn btn-light" style="cursor: pointer;" onclick="gotop()" id="goTop"><i class="fas fa-arrow-up"></i> Về đầu trang</span>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <hr class="mt-3 text-light">
+            <div class="col-md-12">
+                <p class="text-lg-start text-center text-light light-300">
+                    Bản quyền @2023 , Công ty TNHH Đầu tư & Phát triển FinTop
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
