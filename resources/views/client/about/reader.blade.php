9ae083288bc34aee15c8542161c587a90fd147e6
luatnc <luatncth2007024@fpt.edu.vn>
2023-06-14 10:36:52 -0700

    fix-css

--- resources/views/client/about/reader.blade.php
+++ resources/views/client/about/reader.blade.php
@@ -1,6 +1,6 @@
 <div class="modal-dialog modal-xl">
     <div class="modal-content">
-        <div class="modal-header" style="background:#299c03cc">
+        <div class="modal-header" style="background:#101826eb">
             <h5 class="modal-title" style="color:white">{{ $datas['blogDetail']->title }}</h5>
             <button style="background:white" type="button" class="btn btn-sm" data-bs-dismiss="modal">
                 X
